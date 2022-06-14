6a0ab620ef7665d3e6d441f597ad236dc6cd2015
uturkarslan <ugurturkarslan@gmail.com>
2022-06-14 18:43:30 +0200

    Add runner

---
+++ run_demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set application name
+appName="demo"
+
+# Build Docker image
+docker build \
+  --build-arg newRelicAppName=$appName \
+  --build-arg newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
+  --tag $appName \
+  "./demo/."
+
+# Run application as Docker container
+docker run \
+  -d \
+  --rm \
+  --name $appName \
+  -p 8080:8080 \
+  $appName
+
+while true
+do
+  response=$(curl http://localhost:8080)
+  echo $response
+  sleep 3
+done
